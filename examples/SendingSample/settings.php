fef8f1d771ce6fd94205c26fefa687bb3ae5c3c0
Long Do <tamlong@tctav.com>
2016-07-05 10:24:33 +0700

    123

---
+++ examples/SendingSample/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'channelId' => '1473054263',
+    'channelSecret' => '0e457eef6c9f5e9bac5a15e05db223c4',
+    'channelMid' => 'u5e82a2ae5a724318f7d205731dac1083',
+    'targetMid' => 'u2677dbbe1a522044242223a1ccbe2f23',
+
+];
